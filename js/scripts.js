a6c8490ac22705de04d6875c4397c844b6bd772e
Nathan Wentworth <nwentworth95@gmail.com>
2017-05-21 18:30:38 -0400

    added wanikani, added auto-refresh

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,11 @@
 
 // gmail api 171868967722-8hgapsq2vvu9v6aqgud3vgi5v9n3apkp.apps.googleusercontent.com
 
+// wanikani api 4b9e1ddef7f0f6d5ce310ceecf0f7a41
+
 var lastTimeTempAccessed;
-var lastTimeGmailAccessed;
 var temp;
+var reviewNo;
 
 var CLIENT_ID = '171868967722-8hgapsq2vvu9v6aqgud3vgi5v9n3apkp.apps.googleusercontent.com';
 var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest"];
@@ -21,14 +23,15 @@ function init() {
   var n = d.getTime();
 
   lastTimeTempAccessed = localStorage.getItem('lastTimeTempAccessed');
-  lastTimeGmailAccessed = localStorage.getItem('lastTimeGmailAccessed');
   if (lastTimeTempAccessed != null) {
     if (lastTimeTempAccessed < (n - 600000)) {
       console.log("it's been more than 10 minutes since the weather was accessed, getting new data");
       getWeatherData();
+      getWaniKaniData();
     } else {
       console.log('lastTimeTempAccessed is less than 10 minutes ago: ' + lastTimeTempAccessed);
       setTempDisplay();
+      setWaniKaniDisplay();
     }
   } else {
     console.log("localStorage version of lastTimeTempAccessed is null: " + lastTimeTempAccessed);
@@ -39,12 +42,24 @@ function init() {
   localStorage.setItem('lastTimeTempAccessed', lastTimeTempAccessed);
   console.log('lastTimeTempAccessed was ' + lastTimeTempAccessed);
 
-  setInterval(function() {
-    getTime(d), 1000
-  });
+  window.setInterval(function () {
+    getTime();
+  }, 1000);
+
+  window.setInterval(function () {
+    getWeatherData();
+  }, 1800000);
+
+  window.setInterval(function () {
+    getWaniKaniData();
+  }, 3600000);
 }
 
-function getTime(d) {
+
+
+function getTime() {
+  var d = new Date();
+
   var h = d.getHours();
   var m = d.getMinutes();
   h = h % 12;
@@ -60,6 +75,37 @@ function getTime(d) {
 function displayTime(h, m) {
   var timeEl = document.getElementById('time');
   timeEl.innerText = h + ":" + m;
+}
+
+function getWaniKaniData() {
+  getJSON('https://www.wanikani.com/api/user/4b9e1ddef7f0f6d5ce310ceecf0f7a41/study-queue',
+    function(err, data) {
+      if (err != null) {
+        console.log('Something went wrong: ' + err);
+      } else {
+        console.log('success!');
+        getWaniKaniStudyNo(data);
+        setWaniKaniDisplay();
+      }
+    }
+  );
+}
+
+function getWaniKaniStudyNo(data) {
+  reviewNo = data.requested_information.reviews_available;
+  localStorage.setItem('wanikani', reviewNo);
+}
+
+function setWaniKaniDisplay() {
+  reviewNo = localStorage.getItem('wanikani');
+
+  if (reviewNo == null) {
+    console.log("couldn't find cached wanikani, getting new data");
+    getWaniKaniData();
+  }
+
+  wanikani = document.getElementById('wanikani');
+  wanikani.innerText = reviewNo;
 }
 
 function getWeatherData() {
@@ -138,6 +184,11 @@ function initClient() {
 function updateSigninStatus(isSignedIn) {
   if (isSignedIn) {
     displayUnread();
+
+    window.setInterval(function () {
+      displayUnread(), 900000
+    });
+
   } else {
     handleAuthClick();
   }
